dd3440bb410ba22d64c4766318ce01083a6e8903
unknown <mail@felixmelchner.de>
2020-05-16 16:46:07 +0200

    added mobile gallery optimizations

--- components/gallery/Gallery.js
+++ components/gallery/Gallery.js
@@ -3,111 +3,42 @@ import styles from "./Gallery.module.scss"
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import { MdKeyboardArrowRight } from 'react-icons/md'
 import Link from "next/link"
+import Desktopview from "./DesktopView"
+import Mobileview from "./MobileView"
 
-export default class Footer extends Component {
+export default class Gallery extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { height: 512 };
+        this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+    }
+    
+    componentDidMount() {
+        this.updateWindowDimensions();
+        window.addEventListener("resize", this.updateWindowDimensions.bind(this));
+    }
+    
+    componentWillUnmount() {
+        window.removeEventListener("resize", this.updateWindowDimensions.bind(this));
+    }
+    
+    updateWindowDimensions() {
+        this.setState({ width: window.innerWidth, height: window.innerHeight });
+    }
   render() {
+    let view;  
+    if(this.state.width<701)
+      {
+          view = <Mobileview/>
+      }
+      else
+      {
+          view = <Desktopview/>
+      }
     return (
       <>
         <div className={styles.wrapper}>
-            <div className={styles.row}>
-                <div className={styles.column}>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_8124-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_7994-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_8438-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_8347-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_7476-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_7572-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_4388-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_4508-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_4010-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_4034-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_4190-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_7017-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_3255-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_7070-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_9459-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_9656-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_4684-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_6574-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_6137-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_6245-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_7696-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_6761-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_8556-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_7917-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_4272-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_4217-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_9929-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_1140-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_0761-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_2103-insta.jpg")}/>
-                    </div>
-                    <div className={styles.column}>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_8060-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_8448-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_8273-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_8306-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_7860-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_7578-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_4352-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_4534-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_4202-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_2895-2-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_3803-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_4608-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_9123-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_9686-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_4966-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_5708-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_6086-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_6493-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_5468-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_7696-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_9239-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_7847-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_7052-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_8664-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_9819-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_0812-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_0821-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_0623-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_0206-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_1980-viktoria.jpg")}/>
-
-                    </div>
-                    <div className={styles.column}>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_8045-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_8484-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_7395-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_8478-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_7519-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_4445-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_4525-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_3952-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_4272-insta-1.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_6906-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_3813-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_4672-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_9561-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_9485-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_5020-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_6371-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_5206-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_5450-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_7336-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_6339-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_8378-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_8504-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_8523-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_4272-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_0738-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_0774-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_0663-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_9965-viktoria.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_9686-3-insta.jpg")}/>
-                    <LazyLoadImage effect="opacity" src={require("../../public/assets/travel/IMG_2059-insta.jpg")}/>
-                </div>
-            </div>
+            {view}
             <Link href="/portrait">
                 <a className={styles.next} >Next Gallery<MdKeyboardArrowRight className={styles.arrowicon}/></a>
             </Link>
