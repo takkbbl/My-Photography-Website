f270ad57146e290d48b503aeb31021a2b1958432
Felix Melchner <felix.melchner@studium.uni-erlangen.de>
2020-05-22 23:11:34 +0200

    finished contact form

--- pages/Contact.js
+++ pages/Contact.js
@@ -3,6 +3,7 @@ import styles from "./Contact.module.scss";
 import photo from "../public/assets/felixmelchner-contact.jpg"
 import React, { Component } from "react";
 import axios from 'axios';
+import Link from "next/link";
 
 
 export default class Contact extends Component{  
@@ -11,7 +12,8 @@ export default class Contact extends Component{
     this.state = {
       name: '',
       email: '',
-      message: ''
+      message: '',
+      sent: 'Send message'
     };
   }
   myChangeHandler = (event) => {
@@ -20,33 +22,23 @@ export default class Contact extends Component{
     this.setState({[nam]: val});
   }
 
-  mySubmitHandler = (event) => {
-    event.preventDefault();
-    alert("Name: " + this.state.name);
-    alert("Email: " + this.state.email);
-    alert("Message " + this.state.message);
-  }
-
   handleSubmit(e){
     e.preventDefault();
-    alert("handlesubmit");
+    this.setState({sent: 'Sending message...'});
     axios({
       method: "POST", 
       url:"https://agile-citadel-49078.herokuapp.com/send", 
       data:  this.state
     }).then((response)=>{
       if (response.data.status === 'success'){
-        alert("Message Sent."); 
         this.resetForm()
       }else if(response.data.status === 'fail'){
-        alert("Message failed to send.")
       }
     })
   }
 
   resetForm(){
-    
-    this.setState({name: '', email: '', message: ''})
+    this.setState({message: '', email: '', name: '', sent: 'Message sent!'})
  }
 
   render(){
@@ -59,29 +51,40 @@ export default class Contact extends Component{
               <img src={photo} alt="Felix Melchner"></img>
             </div>
             <div className={styles.contact}>
-              <h1>Contact me</h1>
-              <p>Are you interested in working together? Do you have any questions or just want to say hi? Drop me a line by filling out the contact form below and I will get back to you as soon as I can.</p>
-              <div className={styles.form}>
-              <form onSubmit={this.handleSubmit.bind(this)} method="POST">
-                <p>Enter your name:</p>
-                <input
-                  type='text'
-                  name='name'
-                  onChange={this.myChangeHandler}
-                />
-                <p>Your email:</p>
-                <input
-                  type='text'
-                  name='email'
-                  onChange={this.myChangeHandler}
-                />
-                <p>Your Message:</p>
-                <textarea name='message' type='text' value={this.state.message} onChange={this.myChangeHandler} />
-                <input
-                  type='submit'
-                />
-                </form>
-                <a href="https://blog.mailtrap.io/react-contact-form/" target="_blank">Continue here</a>
+              <div className={styles.innercontact}>
+                <h1>Contact me</h1>
+                <p>Are you interested in working together? Do you have any questions or just want to say hi? Drop me a line by filling out the contact form below and I will get back to you as soon as I can.</p>
+                <div className={styles.form}>
+                <form onSubmit={this.handleSubmit.bind(this)} method="POST">
+                  <input
+                    type='text'
+                    className={styles.inputname}
+                    name='name'
+                    required
+                    placeholder='Name'
+                    onChange={this.myChangeHandler}
+                  />
+                  <input
+                    type='email'
+                    name='email' 
+                    required
+                    className={styles.inputmail}
+                    placeholder='Email'
+                    onChange={this.myChangeHandler}
+                  />
+                  <textarea name='message' required className={styles.inputmessage} placeholder='Leave me a message' type='text' value={this.state.message} onChange={this.myChangeHandler} />
+                  <input type="checkbox" id="gdpr" className={styles.gdpr} required name="gdpr" value="Gdkjsfkl"></input>
+                  <label className={styles.gdprlabel} for="gdpr"> I consent to having this website store my submitted information so they can respond to my inquiry.
+See my <Link href={"/privacy-policy"}>
+                  <a target="_blank">Privacy Policy</a>
+                </Link> to learn more about how I use data.</label><br></br>
+                  <input
+                    type='submit'
+                    value={this.state.sent}
+                    className={styles.submitbutton}
+                  />
+                  </form>
+                </div>
               </div>
             </div>
           </div>
